77373a6f16c6085c1a589366de14ff8b7f6d890c
Andy Lukas <kievecrash@gmail.com>
2014-11-12 15:41:17 -0800

    Initial upload to github (hopefully)

---
+++ Assets/Standard Assets/scripts/plat_toggle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plat_toggle : MonoBehaviour {
+	public bool isActive = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftControl)){
+			if(isActive){
+				this.collider.enabled =false;
+				isActive = false;
+			}
+			else{
+				this.collider.enabled = true;
+				isActive = true;
+			}
+		}
+	}
+}
---
+++ Assets/Standard Assets/scripts/shift_script.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shift_script : MonoBehaviour {
+public bool isOn = false;
+public float duration = 1.0F;
+private bool isScaling = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftControl)){
+			if(isOn){
+				isOn = false;
+				StartCoroutine (DoOtherScaleThing());
+			}
+			else{
+				isOn = true;
+				StartCoroutine (DoScaleThing());
+			}
+		}
+	}
+
+
+	public IEnumerator DoOtherScaleThing(){
+		yield return new WaitForSeconds(0.5F);
+		transform.localScale = new Vector3(0.00001F,0.00001F,0.00001F);
+		yield break;
+	}
+
+	public IEnumerator DoScaleThing(){
+		if (isScaling)
+			yield break;
+
+		isScaling = true;
+		float startTime = Time.time;
+
+		while (Time.time - startTime < duration){
+			transform.localScale += new Vector3(0.05F,0.05F,0.05F);
+			yield return null;
+		}
+
+//		for(int i = 1; i <= 1000; i ++){
+//			transform.localScale += new Vector3((1/i),(1/i),(1/i));
+//			yield return null;
+//		}
+
+		isScaling = false;
+	}
+}
